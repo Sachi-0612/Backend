32354854b4666b5381d363bb32ffdbf6e59837ef
Sachi-0612 <sachikaursidhu006@gmail.com>
2026-04-02 13:01:53 +0530

    auth completed: token verification

--- auth/src/routes/post.route.js
+++ auth/src/routes/post.route.js
@@ -1,11 +1,35 @@
+require('dotenv').config()
 const express = require('express')
+const jwt = require("jsonwebtoken")
+const userModel = require('../models/user.model')
 
 
 const router = express.Router()
 
-router.post('/create', (req, res)=>{
+router.post('/create', async (req, res)=>{
     console.log(req.body)
+    const token = req.cookies.token
 
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+    
+    try{        
+       const decoded = jwt.verify(token, process.env.JWT_SECRET)
+       console.log("decoded:",decoded);
+
+       const user = await userModel.findOne({
+        _id: decoded.id
+       })
+       console.log(user)
+    }catch(err){
+        return res.status(401).json({
+            message: "Token is invalid"
+        })
+    }
+ 
     console.log(req.cookies)
 
     res.send("Post created successfully")
